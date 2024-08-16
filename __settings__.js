cb0483b0d32d6adf517caf1fa312506a07eab794
Maurichilean3d <Maurichilean3d@users.noreply.github.com>
2024-08-16 01:36:30 -0400

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2053511.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 192566532, 192566533, 192566534, 192566535 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/192566556/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/192566557/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/192566555/1/ammo.js', 'preload' : true},
+];
